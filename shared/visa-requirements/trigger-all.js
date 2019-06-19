e93009f8caff23367ed86736536735e4f503c006
Christine Cha <cha@Christines-MacBook-Pro.local>
2019-06-18 21:37:27 -0400

    Added more country data.

--- shared/visa-requirements/trigger-all.js
+++ shared/visa-requirements/trigger-all.js
@@ -13,20 +13,53 @@ if (!fs.existsSync(REQUIREMENTS_FOLDER)) {
   fs.mkdirSync(REQUIREMENTS_FOLDER)
 }
 
-countriesJSON.forEach(country => {
-  if (!country.wikipediaSource) {
-    return
-  }
+const universalPolicies = countriesJSON
+.filter(country => !!country.visaPolicy)
+.map(country => country.visaPolicy)
 
+const queue = []
+const MAX_CONCURRENT = 10
+
+const storeCountryData =  country => {
+  console.log('>> Fetching visa requirements for:', country.code, country.name)
+  
   getVisaRequirements(country)
   .then((requirements = []) => {
     const data = {}
+
     requirements.forEach(r => {
       data[r.destination_country] = r
+    })
+
+    universalPolicies.forEach(r => {
+      data[r.destination_country] = { ...r, passport_country: country.code }
     })
 
     const filepath = path.resolve(REQUIREMENTS_FOLDER, `${country.code}.json`)
     
     jsonfile.writeFileSync(filepath, data, { spaces: 2 })
+
+    if (queue.length > 0) {
+      const next = queue.shift()
+      storeCountryData(next)
+    }
   })
+}
+
+countriesJSON.forEach((country, i) => {
+  if (country.noPassport) {
+    return
+  }
+
+  if (!country.wikipediaSource) {
+    console.log('Missing wikipedia source for:', country.code, country.name)
+    return
+  }
+
+  if (i >= MAX_CONCURRENT) {
+    queue.push(country)
+    return
+  }
+
+  storeCountryData(country)
 })
