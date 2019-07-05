2821b9c72fd2488ff3097aaafbecc2f293fd7cd3
Christine Cha <hello@christinecha.com>
2019-07-05 00:40:19 -0400

    Generate sitemap

---
+++ scripts/generate-sitemap.js
@@ -0,0 +1,17 @@
+const fs = require('fs-extra')
+const path = require('path')
+const pages = require('../pages-config')
+
+const SITEMAP_FILEPATH = path.resolve(__dirname, '../public/sitemap.txt')
+
+const URLS = [
+  'https://can-i-go-there.com',
+]
+
+pages.forEach((page) => {
+  const slug = page.filename.split('.html')[0]
+  if (slug === 'index') return
+  URLS.push(`https:/can-i-go-there.com/${slug}`)
+})
+
+fs.writeFileSync(SITEMAP_FILEPATH, URLS.join('\n'))
